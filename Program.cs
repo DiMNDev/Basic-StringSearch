f8e24c7ca62e8c6fd0961f93616c6e6725486226
DiMNDev <Dimndev@gmail.com>
2024-09-15 10:00:32 -0600

    finished simple search

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+string[] arrayOfStrings = ["banana", "orange", "apple", "macintosh", "star", "bilbo", "gandalf", "Helly R.", "Mark S.", "goose", "duck", "llama", "spruce", "diamond", "carmen sandiego"];
+
+string SearchFor(string word, string[] wordArray)
+{
+    string returnMessage = $"{word} not found";
+
+    for (int i = 0; i < wordArray.Length; i++)
+    {
+        if (wordArray[i] == word)
+        {
+            returnMessage = $"{word} was found at index {i}";
+        }
+    }
+
+    return returnMessage;
+}
+
+Console.WriteLine(SearchFor("carmen sandiego", arrayOfStrings));
+Console.WriteLine(SearchFor("waldo", arrayOfStrings));
